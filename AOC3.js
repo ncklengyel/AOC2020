6e7ea97dfdccfae99e2418b6afd72d189fcf902a
Nicolas Lengyel <nlengyel@centumadetel.com>
2020-12-22 04:43:16 -0500

    Day 3 - Part 1

--- AOC3.js
+++ AOC3.js
@@ -1,25 +1,30 @@
 var fs = require('fs');
 
 function part1(input) {
+    let trees = 0, tree = '#', index = 0, increment = 3;
+    input.shift(); // remove first line
+    input.forEach(line => {
+        if (line != '') {
+            index = index + increment;
+            index = index >= line.length ? index - line.length : index;
+            trees = line[index] == tree ? trees + 1 : trees;
+        }
+    });
 
+    console.log("Part 1 :", trees);
 }
 
 function part2(input) {
 
 }
 
-fs.readFile("inputs/input2.txt", "utf8", (err, data) => {
+fs.readFile("inputs/input3.txt", "utf8", (err, data) => {
     
     if (err) 
         throw err;
 
-    let input = [];
-
     // Transform data
-    data.split("\n").forEach(line => {
-        if (line != "") {
-        }
-    });
+    let input = data.split("\n");
 
     part1(input);
     part2(input);
