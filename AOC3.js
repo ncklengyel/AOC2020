a381f135d452e89e78160a1e47b85cc904dfd686
Nicolas Lengyel <nlengyel@centumadetel.com>
2020-12-23 03:24:22 -0500

    Day 3 - Part 2

--- AOC3.js
+++ AOC3.js
@@ -1,30 +1,41 @@
 var fs = require('fs');
 
-function part1(input) {
-    let trees = 0, tree = '#', index = 0, increment = 3;
-    input.shift(); // remove first line
-    input.forEach(line => {
-        if (line != '') {
-            index = index + increment;
-            index = index >= line.length ? index - line.length : index;
-            trees = line[index] == tree ? trees + 1 : trees;
-        }
-    });
+const tree = '#';
 
+function part1(input) {
+    let trees = treesOnSlope(3, 1, input);
     console.log("Part 1 :", trees);
 }
 
 function part2(input) {
+    const slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]];
+    let treeArray = [];
+    slopes.forEach(slope => treeArray.push(treesOnSlope(slope[0], slope[1], input)));
+    console.log("Part 2: ", treeArray.reduce((a, b) => a * b, 1));
+}
 
+function treesOnSlope(rightInc, downInc, input) {
+    let trees = 0;
+    let position = {x: 0, y: 0};
+
+    while (position.y < input.length) {
+        const xx = position.x % input[0].length;
+        let area = input[position.y][xx];
+
+        if (area == tree)
+        {
+            trees++;
+        }
+
+        position.x += rightInc;
+        position.y += downInc;
+    }
+
+    return trees;
 }
 
 fs.readFile("inputs/input3.txt", "utf8", (err, data) => {
-    
-    if (err) 
-        throw err;
-
-    // Transform data
-    let input = data.split("\n");
+    let input = data.split('\n');
 
     part1(input);
     part2(input);
